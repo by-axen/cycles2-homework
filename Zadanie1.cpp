e110343d4f4e796ddb7cd8d8848e14c9260a9400
by-axen <by_axen@live.ru>
2023-03-19 21:53:30 +0300

    добавила программу, которая выводит таблицу умножения на число 5

---
+++ Zadanie1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* https://github.com/by-axen/cycles2-homework.git */
+
+int main()
+{
+	for(int i=1;i<11;i++)
+	{
+		printf("%i*5=%i\n",i,i*5);
+	}
+	printf("\n\n");
+
+	return 0;
+}
+	
